159f3bce53a6cf71735ce4bb35ec22e3a24e7c68
esborni.mattia.07@itisdalmine.edu.it <esborni.mattia.07@itisdalmine.edu.it>
2025-03-06 19:21:50 +0100

    ambiente e oggetto

--- among us/Oggetto.cs
+++ among us/Oggetto.cs
@@ -6,22 +6,48 @@ using System.Threading.Tasks;
 
 namespace Among_us
 {
+    public enum oggetti
+    {
+        attrezzo_multiuso, //stanza 1 e 10
+        materasso_nuovo,   //stanza 4
+        motore_elettrico,  //stanza 5
+        tessera_di_accesso,//stanza 7 
+        tubo_di_ricambio,  //stanza 8
+        batteria,          //stanza 11
+        saldatrice,        //stanza 12
+        fusibile,          //stanza 13
+        scheda_di_rete,    //stanza 15
+        chiave_magnetica,  //stanza 17
+        filtro_acqua,      //stanza 18
+        bombola_aria,      //stanza 19
+        casco_di_ricambio  //stanza 20
+    }
     public class Oggetto
     {
-        private string nome;
+        private oggetti nome;
+        public Ambiente posizione { get; set; }
 
-        public string Nome
+
+        public oggetti Nome
         {
             get { return nome; }
             set
             {
-                if(value.Length<3 || value.Length > 20)
+                if(!(value is oggetti))
                 {
-                    throw new ArgumentException("lunghezza nome non valida");
+                    throw new ArgumentException("oggetto non valido");
                 }
                 nome = value;
             }
         }
+
+        public Oggetto(oggetti nome, Ambiente pos)
+        {
+            Nome = nome;
+            posizione = pos;
+        }
+
+
 
     }
 }
--- among us/Task.cs
+++ among us/Task.cs
@@ -6,22 +6,7 @@ using System.Threading.Tasks;
 
 namespace Among_us
 {
-    public enum oggetti
-    {
-        attrezzo_multiuso, //stanza 1 e 10
-        materasso_nuovo,   //stanza 4
-        motore_elettrico,  //stanza 5
-        tessera_di_accesso,//stanza 7 
-        tubo_di_ricambio,  //stanza 8
-        batteria,          //stanza 11
-        saldatrice,        //stanza 12
-        fusibile,          //stanza 13
-        scheda_di_rete,    //stanza 15
-        chiave_magnetica,  //stanza 17
-        filtro_acqua,      //stanza 18
-        bombola_aria,      //stanza 19
-        casco_di_ricambio  //stanza 20
-    }
+    
     
     public class Task
     {
@@ -54,7 +39,7 @@ namespace Among_us
                 {
                     throw new Exception("la task ha bisogno di un oggetto ma non è stato detto quale");
                 }
-                if (!giocatore.guarda_zaino().ToList().Contains(oggetto.Nome))
+                if (!giocatore.guarda_zaino().ToList().Contains(oggetto.Nome.ToString()))
                 {
                     throw new Exception($" Serve {Oggettonecessario} per svolgere la task");
                 }
