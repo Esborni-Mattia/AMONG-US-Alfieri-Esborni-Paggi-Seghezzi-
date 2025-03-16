9b0fc4bd821fe879c989e8a43d096f3d2e42afe6
Seghezzi <seghezzi.alessandro.07@itisdalmine.edu.it>
2025-03-16 21:38:39 +0100

    mappa 3 (paggi) + doc

--- among us/obj/Debug/net8.0-windows/Among us.AssemblyInfo.cs
+++ among us/obj/Debug/net8.0-windows/Among us.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Among us")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3c2482ca17a5c73b3dd79179f91b95fd109283a0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8abafa4394958b7c0c2abbcb200db1035ab82daa")]
 [assembly: System.Reflection.AssemblyProductAttribute("Among us")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Among us")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
